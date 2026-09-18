9f7814b1155a677c831a9d4974583ee099bb373c
高境 <you@example.com>
2026-09-18 20:27:36 +0800

    指纹改为本地非驻留凭据，无需iCloud钥匙串

--- js/private.js
+++ js/private.js
@@ -114,7 +114,7 @@ async function registerFingerprint() {
       ],
       authenticatorSelection: {
         authenticatorAttachment: "platform",
-        residentKey: "required",
+        residentKey: "discouraged",
         userVerification: "required",
       },
       timeout: 60000,
